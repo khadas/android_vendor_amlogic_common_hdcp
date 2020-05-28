050fcf811edd1601152bd39987c205005fca80a9
Pengguang Zhu <pengguang.zhu@amlogic.com>
2020-05-28 11:11:48 +0800

    mk: update makefile/ta for TDK V3.8 [1/1]
    PD#SWPL-23388
    
    Problem:
    1. ta name is not correct
    
    Solution:
    1. update ta name
    2. add TA for TDK V3.8
    3. optimize Android.mk
    
    Verify:
    Android Q + franklin
    
    Change-Id: I6c8fb24351111678aa8c3d4194a2006c41afa079
    Signed-off-by: Pengguang Zhu <pengguang.zhu@amlogic.com>

--- ca/Android.mk
+++ ca/Android.mk
@@ -1,26 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-OUT_PATH := $(PRODUCT_OUT)/vendor
-else
-OUT_PATH := $(PRODUCT_OUT)/system
-endif
-
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(TARGET_ARCH), arm)
-LOCAL_SRC_FILES := bin/tee_hdcp
-else
-LOCAL_SRC_FILES := bin64/tee_hdcp
-endif
-
-LOCAL_INIT_RC := tee_hdcp.rc
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
-endif
-
+LOCAL_SRC_FILES_32 := bin/tee_hdcp
+LOCAL_SRC_FILES_64 := bin64/tee_hdcp
 LOCAL_MODULE := tee_hdcp
+LOCAL_INIT_RC := tee_hdcp.rc
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(OUT_PATH)/bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
--- ta/Android.mk
+++ ta/Android.mk
@@ -1,46 +1,39 @@
 LOCAL_PATH := $(call my-dir)
+TA_UUID := ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3
+TA_SUFFIX := .ta
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-OUT_PATH := $(PRODUCT_OUT)/vendor
+ifeq ($(PLATFORM_TDK_VERSION), 38)
+PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk_v3
+LOCAL_TA := v3/$(TA_UUID)$(TA_SUFFIX)
 else
-OUT_PATH := $(PRODUCT_OUT)/system
+PLATFORM_TDK_PATH := $(BOARD_AML_VENDOR_PATH)/tdk
+LOCAL_TA := $(TA_UUID)$(TA_SUFFIX)
+endif
+
+ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
+ENCRYPT := 1
+else
+ENCRYPT := 0
 endif
 
 include $(CLEAR_VARS)
-ifeq ($(TARGET_ENABLE_TA_SIGN), true)
-ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
-ENCRYPT := --encrypt=1
-else
-ENCRYPT := --encrypt=0
-endif
-
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28 && echo OK),OK)
-KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH)
-else
-KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/keys/
-endif
-$(info $(shell mkdir $(shell pwd)/$(LOCAL_PATH)/signed))
-$(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
-		--in=$(shell pwd)/$(LOCAL_PATH)/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
-		--out=$(shell pwd)/$(LOCAL_PATH)/signed/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
-		$(ENCRYPT) \
-		$(KEYDIR)))
-
-LOCAL_SRC_FILES := signed/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta
-else
-LOCAL_SRC_FILES := ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta
-endif
-
-LOCAL_MODULE := ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .ta
-LOCAL_MODULE_PATH := $(OUT_PATH)/lib/teetz
+LOCAL_SRC_FILES := $(LOCAL_TA)
+LOCAL_MODULE := $(TA_UUID)
+LOCAL_MODULE_SUFFIX := $(TA_SUFFIX)
 LOCAL_STRIP_MODULE := false
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
+ifeq ($(TARGET_ENABLE_TA_SIGN), true)
+LOCAL_POST_INSTALL_CMD = $(PLATFORM_TDK_PATH)/ta_export/scripts/sign_ta_auto.py \
+		--in=$(shell pwd)/$(LOCAL_MODULE_PATH)/$(TA_UUID)$(LOCAL_MODULE_SUFFIX) \
+		--keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH) \
+		--encrypt=$(ENCRYPT)
+endif
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tee_hdcp_ta
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_REQUIRED_MODULES := ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3
+LOCAL_REQUIRED_MODULES := $(TA_UUID)
 include $(BUILD_PHONY_PACKAGE)
