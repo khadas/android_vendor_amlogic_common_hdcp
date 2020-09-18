3ed8dbc925d649da6e05adfea157715b82a34d34
Xindong Xu <xindong.xu@amlogic.com>
2020-09-18 11:14:40 +0800

    hdcp: fix R compile error [1/2]
    PD#SWPL-32314
    
    Problem:
    compile error when build in R
    
    Solution:
    add LOCAL_SHARED_LIBRARIES to fix compile error
    
    Verify:
    ohm
    
    Change-Id: I71053769547c92dcbd1cfb0960a78f06ce215fec
    Signed-off-by: Xindong Xu <xindong.xu@amlogic.com>

--- ca/Android.mk
+++ ca/Android.mk
@@ -7,5 +7,8 @@ LOCAL_MODULE := tee_hdcp
 LOCAL_INIT_RC := tee_hdcp.rc
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := libteec libcutils
+
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
